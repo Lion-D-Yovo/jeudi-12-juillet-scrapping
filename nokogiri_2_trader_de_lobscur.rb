c60331f659f21e0322aa7b7914b5ee44485b2f86
Lion-D-Yovo <lion.abonnements@gmail.com>
2018-07-12 19:42:51 +0200

    modification

---
+++ nokogiri_2_trader_de_lobscur.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+
+###########################################################################################
+#écris une méthode qui récupère l'adresse email à partir de l'url d'une mairie, par exemple celle de Vauréal
+
+
--- nokogiri_route_de_la_mairie.rb
+++ nokogiri_route_de_la_mairie.rb
@@ -4,50 +4,48 @@ require 'nokogiri'
 ###########################################################################################
 #écris une méthode qui récupère l'adresse email à partir de l'url d'une mairie, par exemple celle de Vauréal
 
-def get_the_email_of_a_townhal_from_its_webpage
-	doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
-    doc.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
+def get_the_email_of_vaureal
+	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
+    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
 	  puts mail.text
     end
 end
-get_the_email_of_a_townhal_from_its_webpage
+
+#puts get_the_email_of_vaureal
 
 ###########################################################################################
 #écris une méthode qui récupère toutes les url de villes du Val d'Oise
 #1 => Je recupere les noms des villes
 
-def get_names_of_val_doise_townhalls
-    doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-    return list_towns = doc.css("p>a").map{ |towns|
-	  towns.text
-    }
+def get_names_towns
+    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    return list_towns = page.css("p>a").map{ |towns| towns.text}
 end
-get_names_of_val_doise_townhalls
+
+get_names_towns
 
 # => Je recupere les sites webs des villes apartir des noms des villes
 #je convertis les majuscules en minuscules et les espaces en "-" pour des urls cleans
-def get_websites_val_doise_townhalls
+def get_websites_towns
     list_towns_websites = []
-    get_names_of_val_doise_townhalls.map{ |town|  "http://annuaire-des-mairies.com/95/"+town.downcase.gsub(' ', '-')+".html"
-    }
-   
+    get_names_towns.map{ |town|  "http://annuaire-des-mairies.com/95/"+town.downcase.gsub(' ', '-')+".html"}
 end
-p get_websites_val_doise_townhalls
+
+get_websites_towns
 
 # => Je recupere les adresses mails des villes a partir des sites webs des villes
 #je 
-def get_all_the_urls_of_val_doise_townhalls(website)
+def urls_to_emails(url)
     
-    doc = Nokogiri::HTML(open(website))
-
-	doc.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
-        puts mail.text
-    end
+    page = Nokogiri::HTML(open(url))
+	email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').map{|mail| mail.text}
 end
 
-get_all_the_urls_of_val_doise_townhalls
+def get_emails_towns
+    get_websites_towns.map {|website| urls_to_emails(website)}
+end
 
-#{ "name" => "nomDeLaVille", "email" => "ville@machin.chose" }
+#puts get_emails_towns
 
 
-puts [get_names_of_val_doise_townhalls, get_websites_val_doise_townhalls]
+puts list = get_names_towns.zip(get_emails_towns).to_h
